1467045b7e508ba960cc3849b8a88d887201104a
Xhevjona <xhevjonamuca@gmail.com>
2026-03-14 06:38:49 -0400

    practice task - functions

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,16 @@ let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+
+let amount1;
+let amount2;
+let amount3;
+
+function calculateAmount() {
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+let total = amount1 + amount2 + amount3;
+document.getElementById('amount').innerText = `The total amount is: ${total}`;
+}
